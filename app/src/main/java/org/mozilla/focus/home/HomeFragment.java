3ec30e64976bbf2a8f9d783ee3366761148bbaf8
rxu <rxu@mozilla.com>
2018-01-15 15:36:38 +0800

    Cancel flowing topsites

--- app/src/main/java/org/mozilla/focus/home/HomeFragment.java
+++ app/src/main/java/org/mozilla/focus/home/HomeFragment.java
@@ -342,7 +342,7 @@ public class HomeFragment extends LocaleAwareFragment implements TopSitesContrac
             }
         }
 
-        topSites.addAll(querySites);
+        /*topSites.addAll(querySites);
         TopSideComparator topSideComparator = new TopSideComparator();
         Collections.sort(topSites, topSideComparator);
 
@@ -351,7 +351,7 @@ public class HomeFragment extends LocaleAwareFragment implements TopSitesContrac
             removeDefaultSites(removeSites);
 
             topSites = topSites.subList(0, TOP_SITES_QUERY_LIMIT);
-        }
+        }*/
 
         this.presenter.setSites(topSites);
         this.presenter.populateSites();
