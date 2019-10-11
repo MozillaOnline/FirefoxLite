b27ad8f00f7292c097768b98771c858d8aede22c
rxu <rxu@mozilla.com>
2019-10-11 10:50:34 +0800

    China build config

--- app/src/main/java/org/mozilla/focus/FocusApplication.kt
+++ app/src/main/java/org/mozilla/focus/FocusApplication.kt
@@ -86,7 +86,7 @@ open class FocusApplication : LocaleAwareApplication() {
         SearchEngineManager.getInstance().init(this)
 
         TelemetryWrapper.init(this)
-        AdjustHelper.setupAdjustIfNeeded(this)
+        //AdjustHelper.setupAdjustIfNeeded(this)
 
         BrowsingHistoryManager.getInstance().init(this)
         ScreenshotManager.getInstance().init(this)
