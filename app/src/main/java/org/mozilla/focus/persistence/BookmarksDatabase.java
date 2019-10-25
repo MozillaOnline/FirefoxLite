d6146ceb0985f8c808c6acf19e70bb2f53806fae
rxu <rxu@mozilla.com>
2019-10-25 10:50:59 +0800

    bookmark migration 1-2

--- app/src/main/java/org/mozilla/focus/persistence/BookmarksDatabase.java
+++ app/src/main/java/org/mozilla/focus/persistence/BookmarksDatabase.java
@@ -1,17 +1,27 @@
 package org.mozilla.focus.persistence;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import android.content.Context;
-import androidx.annotation.NonNull;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
-@Database(entities = {BookmarkModel.class}, version = 1)
+@Database(entities = {BookmarkModel.class}, version = 2)
 public abstract class BookmarksDatabase extends RoomDatabase {
 
     private static volatile BookmarksDatabase instance;
 
     public abstract BookmarkDao bookmarkDao();
+
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            // Since we didn't alter the table, there's nothing else to do here.
+        }
+    };
 
     public static BookmarksDatabase getInstance(@NonNull Context context) {
         if (instance == null) {
