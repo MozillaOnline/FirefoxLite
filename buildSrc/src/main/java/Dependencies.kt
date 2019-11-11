a5c49eaf73d19a6a96cb0cf3a671f2f8439d7004
rxu <rxu@mozilla.com>
2019-11-11 15:00:48 +0800

    bump version to 2.0.3

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -3,8 +3,8 @@ object Versions {
     const val target_sdk = 28
     const val compile_sdk = 28
     const val build_tools = "28.0.3"
-    const val version_code = 2020
-    const val version_name = "2.0.2"
+    const val version_code = 2030
+    const val version_name = "2.0.3"
     const val android_gradle_plugin = "3.4.2"
     const val gms_oss_licenses_plugin = "0.9.3"
     const val support = "1.0.0"
